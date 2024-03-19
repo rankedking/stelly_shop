23afceea7e1230ec82b80261ec8ace71d7a0acb5
Phil <philemon.rsu@gmail.com>
2024-03-19 14:10:51 +0100

    Update components

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# stelly_shop
- A beautiful template for an online shop build with Reactjs
+🎁✨Enjoy the Free code + Assets, Subscribe our youtube channel to get more. 😍
+
+  - Subscribe and Support us on our YouTube Platform: https://my-portfolio-five-iota-33.vercel.app/
+
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { FaMobileAlt } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+} from "react-icons/fa6";
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="dark:bg-gray-950">
+      <div className="container">
+        <div className="grid md:grid-cols-3 pb-20 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <a
+              href="#"
+              className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl
+"
+            >
+              Stelly
+            </a>
+            <p className="text-gray-600 dark:text-white/70  lg:pr-24 pt-3">
+              Stelly is a Italian based online store, You shop and we deliver to your door step
+              faster than you thought.
+            </p>
+            <p className="text-gray-500 mt-4">
+              Developed by PhilDev
+            </p>
+            <a
+              href="https://my-portfolio-five-iota-33.vercel.app/"
+              target="_blank"
+              className="inline-block bg-primary/90 text-white py-2 px-4 mt-4 text-sm rounded-full"
+            >
+              Visit Developer Profile
+            </a>
+          </div>
+
+          {/* Footer links */}
+          <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-bold sm:text-left mb-3">
+                Important Links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* second col links */}
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-bold sm:text-left mb-3">
+                Quick Links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Company Address */}
+            <div className="py-8 px-4 col-span-2 sm:col-auto">
+              <h1 className="text-xl font-bold sm:text-left mb-3">Address</h1>
+              <div>
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Lekki, Phase I, LG Nigeria</p>
+                </div>
+                <div className="flex items-center gap-3 mt-6">
+                  <FaMobileAlt />
+                  <p>+234 808 569 3780</p>
+                </div>
+
+                {/* social links */}
+                <div className="flex items-center gap-3 mt-6">
+                  <a href="#">
+                    <FaInstagram className="text-3xl hover:text-primary duration-300" />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/philblaize">
+                    <FaLinkedin className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
